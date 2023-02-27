46c25795a5ab0d161bd5f5f7ffe3105bfc4ccb83
B1129036 <119582568+XieTangent@users.noreply.github.com>
2023-02-27 21:22:52 +0800

    Update main2.c

--- HW1/main2.c
+++ HW1/main2.c
@@ -1,14 +1,18 @@
 #include <stdio.h>
 
-int main()
-{
-    char a[]="A3B2C4A9";
-    for(int i=0;i<(char)sizeof(a)/sizeof(a[0]);i+=2) {
+void decode(char a[],int len) {
+    for(int i=0;i<len;i+=2) {
         for(int j=0;j<a[i+1]-48;j++) {
             printf("%c",a[i]);
         }
     }
+}
+
+int main()
+{
+    char a[]="A3B2C4A9";
+    int len=(char)sizeof(a)/sizeof(a[0]);
+    decode(a, len);
 
     return 0;
 }
-
