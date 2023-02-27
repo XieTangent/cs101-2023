68300a7575921bd90cc7547d246060898d345f24
B1129036 <119582568+XieTangent@users.noreply.github.com>
2023-02-27 21:34:22 +0800

    Update main3.c

--- HW1/main3.c
+++ HW1/main3.c
@@ -1,18 +1,20 @@
 #include <stdio.h>
 
-int main()
-{
-    char a[]="ABCabcABC";
-    int shift=4;
-    int len=sizeof(a)/sizeof(a[0]);
+void moveleft(char a[],int len,int shift) {
     for(int i=0;i<len;i++,shift++) {
         if(shift==len) {
             shift=0;
         }
         printf("%c",a[shift]);
     }
+}
+
+int main()
+{
+    char a[]="ABCabcABC";
+    int shift=4;
+    int len=sizeof(a)/sizeof(a[0]);
+    moveleft(a,len,shift);
 
     return 0;
 }
-
-
