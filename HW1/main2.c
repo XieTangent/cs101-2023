37032da1a08421810d7e9c1b0e679d7bd87fa0c6
B1129036 <119582568+XieTangent@users.noreply.github.com>
2023-02-27 21:13:46 +0800

    Update main2.c

--- HW1/main2.c
+++ HW1/main2.c
@@ -1,1 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    char a[]="A3B2C4A9";
+    for(int i=0;i<(char)sizeof(a)/sizeof(a[0]);i+=2) {
+        for(int j=0;j<a[i+1]-48;j++) {
+            printf("%c",a[i]);
+        }
+    }
+
+    return 0;
+}
 
