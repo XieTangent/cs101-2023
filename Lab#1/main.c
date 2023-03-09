70dfe452361da9519169c2c99587c7a73e0b6f52
B1129036 <119582568+XieTangent@users.noreply.github.com>
2023-03-09 13:57:53 +0800

    Update main.c

--- Lab#1/main.c
+++ Lab#1/main.c
@@ -1,1 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+int main() {
+    FILE* fp;
+    int a[]={1,2,3};
+    if((fp=fopen("a.bin","wb+"))==NULL) {
+        printf("Cannot open the file");
+        exit(1);
+    }
+    if(fwrite(a,sizeof(a),1,fp)!=1) {
+        printf("File write error");
+    }
+    fseek(fp,0,SEEK_SET);
+    if(fread(a,sizeof(a),1,fp)!=1) {
+        printf("File read error");
+    }
+    for(int i=0;i<3;i++) {
+        printf("%d",a[i]);
+    }
+    
+
+    return 0;
+}
