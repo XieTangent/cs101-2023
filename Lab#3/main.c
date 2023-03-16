319124fb025f2c3c186828961791316357700f35
B1129036 <119582568+XieTangent@users.noreply.github.com>
2023-03-16 14:48:58 +0800

    Create main.c

---
+++ Lab#3/main.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+typedef struct person {
+    int id;
+    char first_name[32];
+    char last_name[32];
+} person_t;
+
+int main()
+{
+    FILE*fp;
+    person_t p[5]={
+        {1,"IU","Lee"},
+        {2,"IUU","Lee"},
+        {3,"IUUU","Lee"},
+        {4,"IUUUU","Lee"},
+        {5,"IUUUUU","Lee"},
+    };
+    person_t tmp;
+    fp=fopen("person.bin","w+");
+    for(int i=0;i<5;i++) {
+        fwrite(p+i,sizeof(person_t),1,fp);
+    }
+    fseek(fp,0,SEEK_SET);
+    for(int i=0;i<5;i++) {
+        fread(&tmp,sizeof(person_t),1,fp);
+        printf("[%d] %d: %s %s\n",i,tmp.id,tmp.first_name,tmp.last_name);
+    }
+    fclose(fp);
+
+    return 0;
+}
