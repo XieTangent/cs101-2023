77752cdf3f13b30796a0c0c709a4be60a792d7d4
B1129036 <119582568+XieTangent@users.noreply.github.com>
2023-04-13 14:12:34 +0800

    Create main.c

---
+++ Lab#6/main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int a(int i,int j) {
+    if(i==9&&j==9) {
+        printf("%d*%d=%d\n",i,j,i*j);
+        return 0;
+    }else if(j==9) {
+        printf("%d*%d=%d\n",i,j,i*j);
+        j=1;
+        a(i+1,j);
+    }else {
+        printf("%d*%d=%d\n",i,j,i*j);
+        a(i,j+1);
+    }
+}
+
+int main()
+{
+    a(1,1);
+
+    return 0;
+}
