6db624436aea4d73aea2559479f243675429edea
B1129036 <119582568+XieTangent@users.noreply.github.com>
2023-05-26 01:17:07 +0800

    Create rmain.cpp

---
+++ Lab#9/rmain.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+class ReplaceMyString {
+    public:
+    ReplaceMyString() {
+        
+    }
+    void replaceString(string oldstr,string newstr) {
+        fstream in("main.cpp");
+        ofstream out;
+        int pos=0;
+        in.open("main.cpp");
+        out.open("rmain.cpp");
+        string str((std::istreambuf_iterator<char>(in)), std::istreambuf_iterator<char>());
+        while(1==1) {
+            pos=str.find(oldstr,pos);
+            if(pos==-1) {
+                pos=0;
+                break;
+            }
+            str.replace(pos,oldstr.length(),newstr);
+            pos+=newstr.length();
+        }
+        out<<str<<endl;
+        in.close();
+        out.close();
+    }
+};
+
+int main() {
+    ReplaceMyString my;
+    my.replaceString("IU is best","IU is best is best");
+
+    return 0;
+}
+
